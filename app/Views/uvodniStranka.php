d915cf4f89b330f5120c1eaaa2889d3947e468bd
blahafilip <fildablazena@gmail.com>
2026-05-12 09:54:40 +0200

    commit 2.2

--- app/Views/uvodniStranka.php
+++ app/Views/uvodniStranka.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <H1>ahoj</H1>
-</body>
-</html>
+<?=$this->extend("layout/template");?>
+
+<?=$this->section("content");?>
+<div>
+    <h1 class="justify-content-center"> ahoj</h1>
+    </div>
+<?=$this->endSection();?>
