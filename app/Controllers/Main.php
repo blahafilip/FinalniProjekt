d994ffd524e84e7b477e259132fd1973dd8bf9aa
cambalrobin <Robincambal@seznam.cz>
2026-05-15 12:35:35 +0200

    comastos

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -4,12 +4,30 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\RaceYear;
+use Config\KonfiguracniSoubor;
+
 
 class Main extends BaseController
 {
     public function index()
     {
-        echo view("uvodniStranka");
+        $config = new KonfiguracniSoubor();
+        $perPage = $config->strankovani;
+
+        $sex = "W";
+        $RaceYear = new RaceYear();
+        $raceV = $RaceYear->where("sex", $sex)->orderBy('year', 'DESC')->paginate($perPage);
+        
+        $pager = $RaceYear->pager;
+         
+        $data = [
+            "infoRace" => $raceV,
+            "pager" => $pager
+        ];
+
+        echo view("uvodniStranka", $data);
+
     }
     public function zavody()
     {
--- app/Views/uvodniStranka.php
+++ app/Views/uvodniStranka.php
@@ -2,43 +2,41 @@
 
 <?=$this->section("content");?>
 <div class="container mt-4">
+        <h3 class="text-center">závody</h3>
+        <?php
+            $table = new \CodeIgniter\View\Table();
 
- 
-    <h3 class="text-center"> závody</h3>
-    <?php
-    $table = new \CodeIgniter\View\Table();
- 
-    $table->setHeading('Parametr', 'Hodnota');
-        foreach($neco as $row)
-        {
-            $table->addRow($row->nazev, $row->hodnota);
-        }
-     
- 
-    $template = array(
-        'table_open'         => '<table class="table table-bordered table-striped">',
-        'thead_open'         => '<thead class="table-dark">',
-        'thead_close'        => '</thead>',
-        'heading_row_start'  => '<tr>',
-        'heading_row_end'    => '</tr>',
-        'heading_cell_start' => '<th>',
-        'heading_cell_end'   => '</th>',
-        'tbody_open'         => '<tbody>',
-        'tbody_close'        => '</tbody>',
-        'row_start'          => '<tr>',
-        'row_end'            => '</tr>',
-        'cell_start'         => '<td>',
-        'cell_end'           => '</td>',
-        'row_alt_start'      => '<tr>',
-        'row_alt_end'        => '</tr>',
-        'cell_alt_start'     => '<td>',
-        'cell_alt_end'       => '</td>',
-        'table_close'        => '</table>'
-    );
- 
-    $table->setTemplate($template);
- 
-    echo $table->generate();
-    ?>
-</div>
-<?=$this->endSection();?>
+            $table->setHeading("Rok","Závod");
+        
+            foreach($infoRace as $row){
+                $table ->addRow( $row->year, $row->real_name);
+            }
+            
+            $template = array(
+                'table_open'=> '<table class="table table-bordered">',
+                'thead_open'=> '<thead>',
+                'thead_close'=> '</thead>',
+                'heading_row_start'=> '<tr>',
+                'heading_row_end'=>' </tr>',
+                'heading_cell_start'=> '<th>',
+                'heading_cell_end' => '</th>',
+                'tbody_open' => '<tbody>',
+                'tbody_close' => '</tbody>',
+                'row_start' => '<tr>',
+                'row_end'  => '</tr>',
+                'cell_start' => '<td>',
+                'cell_end' => '</td>',
+                'row_alt_start' => '<tr>',
+                'row_alt_end' => '</tr>',
+                'cell_alt_start' => '<td>',
+                'cell_alt_end' => '</td>',
+                'table_close' => '</table>'
+                );
+                $table->setTemplate($template);
+                
+            echo $table -> generate();  
+            echo $pager -> links();
+            ?>
+    </div>
+
+<?= $this->endSection(); ?>
